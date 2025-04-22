e32e9b0bd5e94ce7c2cbb7ab1453f3c4ceb32b35
dabideps23 <dabide.ps23@gmail.com>
2025-04-23 01:19:33 +0900

    JS処理の切り出し

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 export const App = () => {
+
+    const onClickButton = () => alert("押下！")
+
     return (
         <>
             <h1>こんにちは</h1>
             <p>お元気ですか</p>
-            <button onClick={() => alert("押下！")}>ボタン</button>
+            <button onClick={onClickButton}>ボタン</button>
         </>
     )
 }

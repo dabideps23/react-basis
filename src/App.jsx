7cbe9c6dd3e889f9e984e1023e7c6007ca1b2046
dabideps23 <dabide.ps23@gmail.com>
2025-04-23 01:21:55 +0900

    スタイル指定の外だし

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,14 @@ export const App = () => {
 
     const onClickButton = () => alert("押下！")
 
+    const contentStyle = {
+        color : "red",
+        fontSize: "30px"
+    }
+    
     return (
         <>
-            <h1 style={{ color: "red" }}>こんにちは</h1>
+            <h1 style={contentStyle}>こんにちは</h1>
             <p>お元気ですか</p>
             <button onClick={onClickButton}>ボタン</button>
         </>

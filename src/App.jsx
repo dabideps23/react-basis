fde58bba06bf27bb2b392576fbafdc6149c56d56
dabideps23 <dabide.ps23@gmail.com>
2025-04-23 12:04:50 +0900

    stateを用いての数値更新

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
+import { useState } from "react";
 import { ColorfulMessage } from "./components/ColorfulMessage"
 
 export const App = () => {
+    const [num, setNum] = useState(0);
 
-    const onClickButton = () => alert("押下！")
-
+    const onClickCountUp = () => {
+        setNum(num + 1);
+    }
 
     return (
         <>
             <h1>こんにちは</h1>
             <ColorfulMessage color="blue" children="お元気ですか？" />
             <ColorfulMessage color="green" children="元気です！" />
-            <button onClick={onClickButton}>ボタン</button>
+            <button onClick={onClickCountUp}>ボタン</button>
+            <p>{num}</p>
         </>
     )
 }

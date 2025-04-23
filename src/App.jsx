4bd40912d9b3db2e175b78b846b788301a2a946f
dabideps23 <dabide.ps23@gmail.com>
2025-04-23 21:36:23 +0900

    フック宣言の宣言場所のルールを確認

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,17 @@ import { useState } from "react";
 import { ColorfulMessage } from "./components/ColorfulMessage"
 
 export const App = () => {
+    //フックの宣言
     const [num, setNum] = useState(0);
 
     const onClickCountUp = () => {
+        //ここでフックの宣言をするとエラー
+        // const [num, setNum] = useState(0);
+
         setNum(num + 1);
-        setNum(num + 1); // 8行目の更新前に評価されるため、相対的に＋１しかしされない。
+        // setNum(num + 1); // 8行目の更新前に評価されるため、相対的に＋１しかしされない。
+        // setNum((prev) => prev + 1);
+        // setNum((prev) => prev + 1);
     }
 
     return (
